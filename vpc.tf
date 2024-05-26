d89d737eaccdea788ee01ae8dcfc9360c104b5cb
SB228516 <149905039+sb228516@users.noreply.github.com>
2024-05-26 09:43:19 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "vpc" {
- source                          = "github.com/sb228516/assignment/blob/master/root/vpc.tf"
+ source                          = "github.com/sb228516/XTIVIA/blob/master/vpc.tf"
     region                          = var.region
     project_name                    = var.project_name
     vpc_cidr                        = var.vpc_cidr
